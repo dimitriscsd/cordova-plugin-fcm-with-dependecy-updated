a17ea6521996936ebd87eaacd714de1b5c74a9be
Dimitrios Matanis <dimitriscsd@users.noreply.github.com>
2024-05-02 11:07:43 +0300

    Update install_ionic_dependencies.bat
    undo security patch to build on windows with node 20 LTS

--- scripts/install_ionic_dependencies.bat
+++ scripts/install_ionic_dependencies.bat
@@ -1,6 +1,6 @@
 cd node_modules
 cd cordova-plugin-fcm-with-dependecy-updated
 cd scripts
-node install_ionic_dependencies.js ionic
-node install_ionic_dependencies.js ionic/ngx
-node install_ionic_dependencies.js ionic/v4
+node install_ionic_dependencies.js ionic --security-revert=CVE-2024-27980
+node install_ionic_dependencies.js ionic/ngx --security-revert=CVE-2024-27980
+node install_ionic_dependencies.js ionic/v4 --security-revert=CVE-2024-27980
